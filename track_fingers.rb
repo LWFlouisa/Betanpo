059c8a66218b067b0929f4d6320d519acb0725e1
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2026-04-28 21:00:55 +0000

    Create track_fingers.rb

---
+++ track_fingers.rb
@@ -0,0 +1,26 @@
+require_relative "DSL/ParallelProcessing.rb"
+
+track_fingers("Ham Sandwhich", "Sandwhich Steak",
+              "John Adam",         "Bombe Smith",
+              "Achovie Pizza",     "Sardine Dip",
+ 
+              "Camera Recorder", "Bathroom Showerhead",
+              "Brown Bookshelf",       "Laptop Webcam",
+              "Multiport Plug",         "Toilet Paper",
+
+              "Oil Pastel",             "Marble Floor",
+              "Ferris Wheel",            "Skate board",
+              "Achovie pizza",       "Cuban sandwhich")
+
+# [ Floating finger: 'Adam Bombe' ]
+# BIANCA: 0.4911 has not reached the highest limit in probability.
+# BIANCA: 0.4911 has not reached the lowest limit in probability.
+# BIANCA: 0.4911 is floating around medium probability.
+# [ Floating finger: 'Plug Toilet' ]
+# BIANCA: 0.34109999999999996 has not reached the highest limit in probability.
+# BIANCA: 0.34109999999999996 has not reached the lowest limit in probability.
+# BIANCA: 0.34109999999999996 is floating around medium probability.
+# [ Floating finger: 'Wheel Skate' ]
+# BIANCA: 0.34109999999999996 has not reached the highest limit in probability.
+# BIANCA: 0.34109999999999996 has not reached the lowest limit in probability.
+# BIANCA: 0.34109999999999996 is floating around medium probability.
