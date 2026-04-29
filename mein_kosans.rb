dbded462232eafd08834325284793b0e5dabbfc8
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2026-04-29 21:36:10 +0000

    Create mein_kosans.rb

---
+++ mein_kosans.rb
@@ -0,0 +1,7 @@
+require "AwasunuDeLingua"
+
+puts "_KOSAN_ [ Asserting Unconfidence ]"
+kosan 0.5, 0.8, 0.2 0.7, 0.8, 0.2, 0.9, 0.8, 0.2
+
+puts "_NIKOSAN_ [ Asserting The Opposite ]"
+nikosan -0.5, -0.2, -0.8, -0.7, -0.2, -0.8, -0.9, -0.2, -0.8
